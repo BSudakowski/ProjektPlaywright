2e5a57122e1a65c499dcca12a3a2dad744102ec6
Your Name <you@example.com>
2024-01-24 22:52:14 +0100

    Run Script Added

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,23 +1,23 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('User login to demobank', () => {
-  const userID = 'testerBS';
   
   test.beforeEach(async ({ page }) => {
     await page.goto('/')
   })
-
-
+  
+  
   test('Login Happy Path', async ({ page }) => {
     //Arange
+    const userID = 'testerBS';
     const userPassword = 'bartekkk';
     const expectedUserName = 'Jan Demobankowy';
-
+    
     //Act
     await page.getByTestId('login-input').fill(userID);
     await page.getByTestId('password-input').fill(userPassword);
     await page.getByTestId('login-button').click();
-
+    
     //Assert
     await expect(page.getByTestId('user-name')).toHaveText(expectedUserName);
   });
@@ -39,6 +39,7 @@ test.describe('User login to demobank', () => {
 
   test('Login unsucesfull - too short password', async ({ page }) => {
     //Arange
+    const userID = 'testerBS';
     const incorrectUserPassword = 'bar';
     const expectedErrorPasswordMessage = 'hasło ma min. 8 znaków';
 
--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -5,7 +5,7 @@ test.describe('Pulpit Tests', () => {
     const userID = 'testerBS';
     const userPassword = 'bartekkk';
 
-    await page.goto('/')
+    await page.goto('/');
     await page.getByTestId('login-input').fill(userID);
     await page.getByTestId('password-input').fill(userPassword);
     await page.getByTestId('login-button').click();
@@ -17,7 +17,7 @@ test.describe('Pulpit Tests', () => {
     const transferAmount = '150';
     const transferTitle = 'pizza';
     const expectedTransferReceiver = 'Chuck Demobankowy';
-
+  
     //Act
     await page.locator('#widget_1_transfer_receiver').selectOption(receiverId);
     await page.locator('#widget_1_transfer_amount').fill(transferAmount);
@@ -47,5 +47,23 @@ test.describe('Pulpit Tests', () => {
 
     //Assert
     await expect(page.locator('#show_messages')).toHaveText(expectedMessage);
+  });
+
+  test('Mobile Top-up Happy Path with ballance check', async ({ page }) => {
+    //Arange
+    const topupReceiver = '502 xxx xxx';
+    const topUpAmount = '19';
+    const initialBalance = await page.locator('#money_value').innerText();
+    const expectedBalance = Number(initialBalance) - Number(topUpAmount);
+
+    //Act
+    await page.locator('#widget_1_topup_receiver').selectOption(topupReceiver);
+    await page.locator('#widget_1_topup_amount').fill(topUpAmount);
+    await page.locator('#uniform-widget_1_topup_agreement span').click();
+    await page.getByRole('button', { name: 'doładuj telefon' }).click();
+    await page.getByTestId('close-button').click();
+
+    //Assert
+    await expect(page.locator('#money_value')).toHaveText(`${expectedBalance}`);
   });
 });
